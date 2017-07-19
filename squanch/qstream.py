5e816f9994bd26b79cf40827cc9ba531548b57d0
Ben Bartlett <benjamincbartlett@gmail.com>
2017-07-19 14:17:40 -0700

    Moved qubit and qsystem to qubit.py; small refactoring

--- squanch/qstream.py
+++ squanch/qstream.py
@@ -1,6 +1,5 @@
 import numpy as np
-import linalg
-import statevector
+import qubit, linalg
 
 
 class QStream:
@@ -15,18 +14,18 @@ class QStream:
         :param systemSize: number of entangled qubits in each quantum system; each system has dimension 2^systemSize
         :param numSystems: number of small quantum systems in the data stream
         '''
-        self.systemSize = systemSize # number of qubits per system
+        self.systemSize = systemSize  # number of qubits per system
         # Initialize each qubit state
-        ketzero = np.array([1,0], dtype = np.complex64) # vector representation of the |0> pure state
-        initialQubitState = np.outer(ketzero, ketzero) # each qubit is initialized as |0><0|
-        initialSystemState = np.array([], dtype=np.complex64)
+        zero = np.array([1, 0], dtype = np.complex64)  # vector representation of the |0> pure state
+        initialQubitState = np.outer(zero, zero)  # each qubit is initialized as |0><0|
+        initialSystemState = np.array([], dtype = np.complex64)
         # Generate the matrix representation of the initial state of the n-qubit system
         for _ in range(systemSize):
             initialSystemState = linalg.tensorProd(initialSystemState, initialQubitState)
 
         # Generate the matrix representation of the overall state of the quantum stream
         # The Hilbert space of QSys0 x QSys1 x ... x QSysN is represented as an n-dimensional array of state vectors
-        self.state = np.repeat([initialSystemState], numSystems, axis=0)  # repeat system state into vertical axis
+        self.state = np.repeat([initialSystemState], numSystems, axis = 0)  # repeat system state into vertical axis
 
         # The "head" of the stream; what qSystem is being processed at the moment
         self.index = 0
@@ -37,7 +36,7 @@ class QStream:
         :param index: zero-index of the quantum system to access
         :return: the state vector of the quantum system
         '''
-        return QSystem(self.state[index], self.systemSize)
+        return qubit.QSystem(self.systemSize, self.state[index])
 
     def pop(self):
         '''
@@ -49,59 +48,59 @@ class QStream:
         return sys
 
 
-class QSystem:
-    '''
-    Represents a multi-particle Hilbert space for several qubuts comprising a single system of a quantum datastream.
-    Designed to have similar syntax to QubitSytem, but instantiation is much faster
-    '''
-
-    def __init__(self, state, numQubits):
-        self.state = state  # density matrix should be passed by reference and will modiy the QStream.state
-        self.numQubits = numQubits
-
-    def qubits(self):
-        '''
-        Return a tuple of qubit objects for indices ranging from 1 to numQubits
-        :return: tuple of qubits in the quantum system
-        '''
-        return (Qubit(self, i) for i in range(self.numQubits))
-
-    def measureQubit(self, qubitIndex):
-        '''
-        Measure the qubit at a given index, partially collapsing the state based on the observed qubit value.
-        The state vector is modified in-place by this function
-        :param qubitIndex: the qubit to measure
-        :return: the measured qubit value
-        '''
-        return statevector.collapse(self.state, qubitIndex, self.numQubits)
-
-    def apply(self, operator):
-        '''
-        Apply an n-qubit operator to this system's n-qubit quantum state
-        :param operator: the *Hermitian* n-qubit operator to apply
-        :return: nothing, the qSystem state is mutated
-        '''
-        # Apply the operator
-        # assert linalg.isHermitian(operator), "Qubit operators must be Hermitian"
-        # self.state[...] = np.linalg.multi_dot([operator, self.state, operator.conj().T])
-        self.state[...] = np.linalg.multi_dot([operator, self.state, operator])
-
-
-class Qubit:
-    '''
-    Represents a single physical qubit, which is part of a pre-allocated nonlocal QSystem
-    '''
-
-    def __init__(self, qSystem, index):
-        self.index = index
-        self.qSystem = qSystem
-
-    def measure(self):
-        return self.qSystem.measureQubit(self.index)
-
-    def getState(self):
-        '''
-        Traces over the remaining portions of the qSystem to return this qubit's state expressed as a density matrix.
-        :return: The (mixed) density matrix describing this qubit's state
-        '''
-        pass
+        # class QSystem:
+        #     '''
+        #     Represents a multi-particle Hilbert space for several qubuts comprising a single system of a quantum datastream.
+        #     Designed to have similar syntax to QubitSytem, but instantiation is much faster
+        #     '''
+        #
+        #     def __init__(self, state, numQubits):
+        #         self.state = state  # density matrix should be passed by reference and will modiy the QStream.state
+        #         self.numQubits = numQubits
+        #
+        #     def qubits(self):
+        #         '''
+        #         Return a tuple of qubit objects for indices ranging from 1 to numQubits
+        #         :return: tuple of qubits in the quantum system
+        #         '''
+        #         return (Qubit(self, i) for i in range(self.numQubits))
+        #
+        #     def measureQubit(self, qubitIndex):
+        #         '''
+        #         Measure the qubit at a given index, partially collapsing the state based on the observed qubit value.
+        #         The state vector is modified in-place by this function
+        #         :param qubitIndex: the qubit to measure
+        #         :return: the measured qubit value
+        #         '''
+        #         return statevector.collapse(self.state, qubitIndex, self.numQubits)
+        #
+        #     def apply(self, operator):
+        #         '''
+        #         Apply an n-qubit operator to this system's n-qubit quantum state
+        #         :param operator: the *Hermitian* n-qubit operator to apply
+        #         :return: nothing, the qSystem state is mutated
+        #         '''
+        #         # Apply the operator
+        #         # assert linalg.isHermitian(operator), "Qubit operators must be Hermitian"
+        #         # self.state[...] = np.linalg.multi_dot([operator, self.state, operator.conj().T])
+        #         self.state[...] = np.linalg.multi_dot([operator, self.state, operator])
+        #
+        #
+        # class Qubit:
+        #     '''
+        #     Represents a single physical qubit, which is part of a pre-allocated nonlocal QSystem
+        #     '''
+        #
+        #     def __init__(self, qSystem, index):
+        #         self.index = index
+        #         self.qSystem = qSystem
+        #
+        #     def measure(self):
+        #         return self.qSystem.measureQubit(self.index)
+        #
+        #     def getState(self):
+        #         '''
+        #         Traces over the remaining portions of the qSystem to return this qubit's state expressed as a density matrix.
+        #         :return: The (mixed) density matrix describing this qubit's state
+        #         '''
+        #         pass
--- squanch/qubit.py
+++ squanch/qubit.py
@@ -1,113 +1,203 @@
 import numpy as np
 import linalg  # useful custom linear algebra functions
+import gates
+
+# Computational basis and projection operators
+_0 = np.array([1, 0], dtype = np.complex64)
+_1 = np.array([0, 1], dtype = np.complex64)
+_M0 = np.outer(_0, _0)
+_M1 = np.outer(_1, _1)
+
+
+class QSystem:
+    '''
+    Represents a multi-particle Hilbert space for several qubuts comprising a single system of a quantum datastream.
+    Designed to have similar syntax to QubitSytem, but instantiation is much faster
+    '''
+
+    def __init__(self, numQubits, state = None):
+        '''
+        Instatiate the quantum state for an n-qubit system
+        :param numQubits: number of qubits in the system, treated as maximally entangled
+        :param state: density matrix representing the quantum state. If none is provided, |000...><...000| is used
+        '''
+        self.numQubits = numQubits
+        # Register the state or generate a new one
+        if state is not None:
+            self.state = state  # density matrix should be passed by reference and will modiy the QStream.state
+        else:
+            # Initialize each qubit state
+            initialQubitState = np.outer(_0, _0)  # each qubit is initialized as |0><0|
+            initialSystemState = np.array([], dtype = np.complex64)
+            # Generate the matrix representation of the initial state of the n-qubit system
+            for _ in range(self.numQubits):
+                initialSystemState = linalg.tensorProd(initialSystemState, initialQubitState)
+
+    def qubits(self):
+        '''
+        Return a tuple of qubit objects for indices ranging from 1 to numQubits
+        :return: tuple of qubits in the quantum system
+        '''
+        return (Qubit(self, i) for i in range(self.numQubits))
+
+    def measureQubit(self, qubitIndex):
+        '''
+        Measure the qubit at a given index, partially collapsing the state based on the observed qubit value.
+        The state vector is modified in-place by this function
+        :param qubitIndex: the qubit to measure
+        :return: the measured qubit value
+        '''
+        measure0 = gates.expandGate(_M0, qubitIndex, self.numQubits, "0")
+        prob0 = np.trace(np.dot(measure0, self.state))
+        # Determine if qubit collapses to |0> or |1>
+        if np.random.rand() <= prob0:
+            # qubit collapses to |0>
+            self.state[...] = np.linalg.multi_dot([measure0, self.state, measure0]) / prob0
+            return 0
+        else:
+            # qubit collapses to |1>
+            measure1 = gates.expandGate(_M1, qubitIndex, self.numQubits, "1")
+            self.state[...] = np.linalg.multi_dot([measure1, self.state, measure1]) / (1.0 - prob0)
+            return 1
+
+    def apply(self, operator):
+        '''
+        Apply an n-qubit operator to this system's n-qubit quantum state: U|psi><psi|U^+ (U^+ = U for Hermitian)
+        :param operator: the *Hermitian* n-qubit operator to apply
+        :return: nothing, the qSystem state is mutated
+        '''
+        # Apply the operator
+        # assert linalg.isHermitian(operator), "Qubit operators must be Hermitian"
+        # self.state[...] = np.linalg.multi_dot([operator, self.state, operator.conj().T])
+        self.state[...] = np.linalg.multi_dot([operator, self.state, operator])
 
 
 class Qubit:
     '''
-    Simple qubit implementation.
-    Retains a reference to a quantum system that can deal with multi-particle dynamics
+    Represents a single physical qubit, which is a wrapper for part of a pre-allocated nonlocal QSystem
     '''
 
-    def __init__(self, initialState=np.array([1, 0])):
-        self.initialState = initialState  # start off in the |0> state by default
-        self.index = 0  # the index/qubit number in the quantum system object
-        self.qSystem = QubitSystem([self])  # the overall quantum state of the system of qubits
-
-    def entangle(self, qSystem):
-        '''
-        Entangle this qubit with another set of ones.
-        This doesn't actually perform any entanglement, it just links the quantum systems
-        together, allowing multi-particle gates like CNOT to be applied.
-        :param qSystem: the state of 1+ qubits with which to entangle this one
-        :return: nothing
-        '''
-        qSystem.addQubit(self)  # add this to the quantum system
-        self.qSystem = qSystem  # change the quantum system object
+    def __init__(self, qSystem, index):
+        self.index = index
+        self.qSystem = qSystem
 
     def measure(self):
-        '''
-        Perform a measurement in the computational (z) basis on this qubit and update the
-        :return: 0 or 1, depending on the outcome of the measurement
-        '''
         return self.qSystem.measureQubit(self.index)
 
-    def apply(self, operator):
+    def getState(self):
         '''
-        Apply a single-qubit operator to this qubit, updating the overall qSystem
-        :param operator: single-qubit operator to apply
-        :return: nothing, the qSystem state is mutated
+        Traces over the remaining portions of the qSystem to return this qubit's state expressed as a density matrix.
+        :return: The (mixed) density matrix describing this qubit's state
         '''
-        self.qSystem.apply(operator, index=self.index)
+        pass
 
-
-class QubitSystem:
-    '''
-    Maintains a multi-particle Hilbert space for several qubits
-    '''
-
-    def __init__(self, qubitList):
-        '''
-        Instantiate a new quantum system object form a list of existing qubits
-        :param qubitList: list of qubits to create the state from
-        '''
-        self.qubits = qubitList
-        self.state = []
-        for i, qubit in enumerate(qubitList):
-            self.state = linalg.tensorProd(self.state, qubit.initialState)
-            qubit.index = i
-
-    def addQubit(self, newQubit):
-        newQubit.index = len(self.qubits)  # zero indexed, so length is next index
-        self.qubits.append(newQubit)
-        # TODO: at the moment you can't make modifications to qubits then entangle them
-        self.state = linalg.tensorProd(self.state, newQubit.initialState)
-
-    def measureQubit(self, qubitIndex):
-        '''
-        Measure the qubit at a given index, partially collapsing the state based
-        on the observed value of the measured qubit
-        :param qubitIndex: the qubit to measure
-        :return: the measured qubit value
-        '''
-        # Generate measurement operators for the n-qubit state
-        M0 = np.outer([1, 0], [1, 0])
-        M1 = np.outer([0, 1], [0, 1])
-        measure0 = linalg.tensorFillIdentity(M0, len(self.qubits), qubitIndex)
-        measure1 = linalg.tensorFillIdentity(M1, len(self.qubits), qubitIndex)
-        prob0 = np.linalg.multi_dot([
-            self.state.conj().T,
-            measure0.conj().T,
-            measure0,
-            self.state
-        ])
-        prob1 = np.linalg.multi_dot([
-            self.state.conj().T,
-            measure1.conj().T,
-            measure1,
-            self.state
-        ])
-        # Perform the measurement and collapse the state matrix accordingly
-        if (np.random.rand() / (prob0 + prob1)) > prob0:
-            # qubit collapses to 1
-            self.state = np.dot(measure1, self.state) / np.sqrt(prob1)
-            return 1
-        else:
-            # qubit collapses to 0
-            self.state = np.dot(measure0, self.state) / np.sqrt(prob0)
-            return 0
-
-    def apply(self, operator, index=None):
-        '''
-        Apply an operator to this quantum state. If no index is specified, an n-qubit
-        operator is assumed, else a single-qubit operation is applied to the specified
-        qubit
-        :param operator: the single- or n-qubit operator to apply
-        :param index: if specified, the index of the qubit to perform the oepration on
-        :return: nothing, the qSystem state is mutated
-        '''
-        # Expand the operator to n qubits if needed
-        if index is not None:
-            operator = linalg.tensorFillIdentity(operator, len(self.qubits), index)
-        # Apply the operator
-        assert linalg.isHermitian(operator), "Qubit operators must be Hermitian"
-        self.state = np.dot(operator, self.state)
+#
+#
+# class Qubit:
+#     '''
+#     Simple qubit implementation.
+#     Retains a reference to a quantum system that can deal with multi-particle dynamics
+#     '''
+#
+#     def __init__(self, initialState=np.array([1, 0])):
+#         self.initialState = initialState  # start off in the |0> state by default
+#         self.index = 0  # the index/qubit number in the quantum system object
+#         self.qSystem = QubitSystem([self])  # the overall quantum state of the system of qubits
+#
+#     def entangle(self, qSystem):
+#         '''
+#         Entangle this qubit with another set of ones.
+#         This doesn't actually perform any entanglement, it just links the quantum systems
+#         together, allowing multi-particle gates like CNOT to be applied.
+#         :param qSystem: the state of 1+ qubits with which to entangle this one
+#         :return: nothing
+#         '''
+#         qSystem.addQubit(self)  # add this to the quantum system
+#         self.qSystem = qSystem  # change the quantum system object
+#
+#     def measure(self):
+#         '''
+#         Perform a measurement in the computational (z) basis on this qubit and update the
+#         :return: 0 or 1, depending on the outcome of the measurement
+#         '''
+#         return self.qSystem.measureQubit(self.index)
+#
+#     def apply(self, operator):
+#         '''
+#         Apply a single-qubit operator to this qubit, updating the overall qSystem
+#         :param operator: single-qubit operator to apply
+#         :return: nothing, the qSystem state is mutated
+#         '''
+#         self.qSystem.apply(operator, index=self.index)
+#
+#
+# class QubitSystem:
+#     '''
+#     Maintains a multi-particle Hilbert space for several qubits
+#     '''
+#
+#     def __init__(self, qubitList):
+#         '''
+#         Instantiate a new quantum system object form a list of existing qubits
+#         :param qubitList: list of qubits to create the state from
+#         '''
+#         self.qubits = qubitList
+#         self.state = []
+#         for i, qubit in enumerate(qubitList):
+#             self.state = linalg.tensorProd(self.state, qubit.initialState)
+#             qubit.index = i
+#
+#     def addQubit(self, newQubit):
+#         newQubit.index = len(self.qubits)  # zero indexed, so length is next index
+#         self.qubits.append(newQubit)
+#         self.state = linalg.tensorProd(self.state, newQubit.initialState)
+#
+#     def measureQubit(self, qubitIndex):
+#         '''
+#         Measure the qubit at a given index, partially collapsing the state based
+#         on the observed value of the measured qubit
+#         :param qubitIndex: the qubit to measure
+#         :return: the measured qubit value
+#         '''
+#         # Generate measurement operators for the n-qubit state
+#         M0 = np.outer([1, 0], [1, 0])
+#         M1 = np.outer([0, 1], [0, 1])
+#         measure0 = linalg.tensorFillIdentity(M0, len(self.qubits), qubitIndex)
+#         measure1 = linalg.tensorFillIdentity(M1, len(self.qubits), qubitIndex)
+#         prob0 = np.linalg.multi_dot([
+#             self.state.conj().T,
+#             measure0.conj().T,
+#             measure0,
+#             self.state
+#         ])
+#         prob1 = np.linalg.multi_dot([
+#             self.state.conj().T,
+#             measure1.conj().T,
+#             measure1,
+#             self.state
+#         ])
+#         # Perform the measurement and collapse the state matrix accordingly
+#         if (np.random.rand() / (prob0 + prob1)) > prob0:
+#             # qubit collapses to 1
+#             self.state = np.dot(measure1, self.state) / np.sqrt(prob1)
+#             return 1
+#         else:
+#             # qubit collapses to 0
+#             self.state = np.dot(measure0, self.state) / np.sqrt(prob0)
+#             return 0
+#
+#     def apply(self, operator, index=None):
+#         '''
+#         Apply an operator to this quantum state. If no index is specified, an n-qubit
+#         operator is assumed, else a single-qubit operation is applied to the specified
+#         qubit
+#         :param operator: the single- or n-qubit operator to apply
+#         :param index: if specified, the index of the qubit to perform the oepration on
+#         :return: nothing, the qSystem state is mutated
+#         '''
+#         # Expand the operator to n qubits if needed
+#         if index is not None:
+#             operator = linalg.tensorFillIdentity(operator, len(self.qubits), index)
+#         # Apply the operator
+#         assert linalg.isHermitian(operator), "Qubit operators must be Hermitian"
+#         self.state = np.dot(operator, self.state)
