f92d3937794fd7a4e9d20597fa1f7ad612df925d
Ben Bartlett <benjamincbartlett@gmail.com>
2018-08-21 17:46:39 -0700

    1.1 release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="SQUANCH",
-    version="1.0.0",
+        version = "1.1.0",
     author="Ben Bartlett",
     author_email="benbartlett@stanford.edu",
     description="Simulator for Quantum Networks and Channels",
